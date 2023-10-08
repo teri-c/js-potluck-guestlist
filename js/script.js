ddd794022ff2ce7b40ba9d5b2825aef520edb128
teri.c <teri.charles23@gmail.com>
2023-10-08 15:38:55 -0400

    Created a potluck guest list

--- js/script.js
+++ js/script.js
@@ -10,3 +10,48 @@ const guestList = document.querySelector(".guest-list");
 const guestCount = document.querySelector(".attendance");
 // alert when guest list is full (not yet visible)
 const guestFull = document.querySelector(".alert");
+
+//add event listener and create an element
+
+addGuestButton.addEventListener("click", function () {
+    const guest = guestInput.value;
+    //console.log(guest); used to test the guest variable 
+
+    if (guest !== "") {
+        addToList(guest);
+        clearInput();
+        updateGuestCount();
+       /* let listItem = document.createElement("li");
+        listItem.innerText = guest;
+        guestList.append(listItem); */
+    }
+});
+
+// clear the input
+
+const clearInput = function () {
+    guestInput.value = "";
+};
+
+// refactor the code
+
+const addToList = function (guest) {
+    const listItem = document.createElement("li");
+    listItem.innerText = guest;
+    guestList.append(listItem);
+};
+
+// limit the guest list
+
+const updateGuestCount = function () {
+    const guests = document.querySelectorAll(".guest-list li");
+    guestCount.innerText = guests.length;
+    
+    if (guests.length === 8) {
+        addGuestButton.classList.add("hide");
+        guestInput.classList.add("hide");
+        guestInputLabel.classList.add("hide");
+        guestFull.classList.remove("hide");
+    }
+
+};
